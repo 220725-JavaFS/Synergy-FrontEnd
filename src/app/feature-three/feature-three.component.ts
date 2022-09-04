2e5d11955e5718ff7cff695f6bb8879f4db7a6a4
JonMokooJang <110052867+JonMokooJang@users.noreply.github.com>
2022-09-04 19:27:44 -0400

    Initial frontend

---
+++ src/app/feature-three/feature-three.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-three',
+  templateUrl: './feature-three.component.html',
+  styleUrls: ['./feature-three.component.css']
+})
+export class FeatureThreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
