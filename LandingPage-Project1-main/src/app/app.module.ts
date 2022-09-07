ac3da1e1208f4e501660942f4b03992e7ceed0b8
Max Morales <maxyMorales52@gmail.com>
2022-09-06 21:59:39 -0400

    Added the front end web page

---
+++ LandingPage-Project1-main/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FeatureOneComponent } from './feature-one/feature-one.component';
+import { FeatureTwoComponent } from './feature-two/feature-two.component';
+import { FeatureThreeComponent } from './feature-three/feature-three.component';
+import { FaqComponent } from './faq/faq.component';
+import { FooterComponent } from './footer/footer.component';
+import { SelectorComponent } from './selector/selector.component';
+import { RegisterComponent } from './register/register.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    FeatureOneComponent,
+    FeatureTwoComponent,
+    FeatureThreeComponent,
+    FaqComponent,
+    FooterComponent,
+    SelectorComponent,
+    RegisterComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
